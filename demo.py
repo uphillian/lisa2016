5a583da7b9039b637747353fe445ec0fe80815e1
Thomas Uphill <thomas@narrabilis.com>
2016-11-06 21:15:19 -0800

    attempt at python for script

---
+++ demo.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+
+#import SimpleHTTPServer
+#def do_GET(self):
+#  parsed_path = urlparse.urlparse(self.path)
+#  try:
+#    params = dict([p.split('=') for p in parsed_path[4].split('&')])
+#    except:
+#        params = {}
+
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+import urlparse
+import urllib
+import os
+
+PORT_NUMBER = 8080
+
+#This class will handles any incoming request from
+#the browser 
+class myHandler(BaseHTTPRequestHandler):
+    
+  #Handler for the GET requests
+  def do_GET(self):
+    parsed_path = urlparse.urlparse(self.path)
+    # if the path matches a file, return it.
+    try:
+      path = parsed_path.path.lstrip('/')
+      if os.path.exists(path):
+        self.send_response(200)
+        self.send_header('Content-type','image/png')
+        self.end_headers()
+        f = open(path,'r')
+        self.wfile.write(f.read())
+        return
+    except:
+      pass
+
+    params = {
+      'title': 'Demo',
+      'text': 'Sample text here'
+    }
+    try:
+      params.update(dict([p.split('=') for p in parsed_path[4].split('&')]))
+    except:
+      pass
+    self.send_response(200)
+    self.send_header('Content-type','text/html')
+    self.end_headers()
+    # Send the html message
+    title=urllib.unquote(params['title'])
+    text=urllib.unquote(params['text'])
+    self.wfile.write('''
+<html>
+<head><title>%s</title></head>
+<body>
+ <h1>%s</h1>
+ <blockquote>%s<blockquote><br>
+ <iframe src="http://localhost:42000/tmux" width=80%% height=70%%></iframe><br>
+ <img src="http://10.1.1.17:8080/lisa2016.png" height=5%%>
+</body>
+</html>
+''' % ( title, title, text ) )
+    return
+
+try:
+    #Create a web server and define the handler to manage the
+    #incoming request
+    server = HTTPServer(('', PORT_NUMBER), myHandler)
+    print 'Started httpserver on port ' , PORT_NUMBER
+    
+    #Wait forever for incoming htto requests
+    server.serve_forever()
+
+except KeyboardInterrupt:
+    print '^C received, shutting down the web server'
+    server.socket.close()
